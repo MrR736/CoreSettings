5357b6949b0a0d792f07275402dc9f38bde90f03
MrR736 <mrr6451@outlook.com>
2024-08-25 11:34:30 -0700

    Update procexp-ReplaceTaskMgr-Enable.cmd

--- procexp-ReplaceTaskMgr-Enable.cmd
+++ procexp-ReplaceTaskMgr-Enable.cmd
@@ -1,4 +1,3 @@
 @echo off
 reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /f /v "Debugger" /d "%Core%\CoreSotfwares\Sysinternals\PROCEXP.EXE"
 reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /f /v "Debugger" /d "%Core%\CoreSotfwares\Sysinternals\PROCEXP.EXE"
-exit
