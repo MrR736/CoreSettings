23c2b3e1b297392351574143205588d50592880e
MrR736 <mrr6451@outlook.com>
2024-08-25 06:05:28 -0700

    Add files via upload

--- procexp-ReplaceTaskMgr-Enable.cmd
+++ procexp-ReplaceTaskMgr-Enable.cmd
@@ -1,4 +1,4 @@
 @echo off
-cd /d %~dp0
-regedit /s procexp-ReplaceTaskMgr-Enable.reg
+reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /f /v "Debugger" /d "%Core%\CoreSotfwares\Sysinternals\Procmon.exe"
+reg.exe add "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /f /v "Debugger" /d "%Core%\CoreSotfwares\Sysinternals\Procmon.exe"
 exit
