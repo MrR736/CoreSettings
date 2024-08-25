13c4400e7f51ec2298c2178a504251d99545ca44
MrR736 <mrr6451@outlook.com>
2024-08-25 06:35:17 -0700

    Update procexp-ReplaceTaskMgr-Disable.cmd

--- procexp-ReplaceTaskMgr-Disable.cmd
+++ procexp-ReplaceTaskMgr-Disable.cmd
@@ -1,4 +1,3 @@
 @echo off
 reg.exe delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /v Debugger /f
 reg.exe delete "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /v Debugger /f
-exit
