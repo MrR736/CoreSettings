8f4f5150c91e2739b997fcad481e4cd118961de6
MrR736 <mrr6451@outlook.com>
2024-08-25 18:46:33 -0700

    Add files via upload

---
+++ CV.cmd
@@ -0,0 +1,17 @@
+@echo off
+set VERSION-CP=0.0.0.0.1
+echo Core Panel
+echo Version %VERSION-CP%
+echo:
+set VERSION-CU=0.0.0.0.2
+echo CoreUpdates
+echo Version %VERSION-CU%
+echo:
+set VERSION-CSe=0.0.0.0.1
+echo CoreSettings
+echo Version %VERSION-CSe%
+echo:
+set VERSION-CSo=0.0.0.0.1
+echo CoreSotfwares
+echo Version %VERSION-CSo%
+@echo on
