ee67e49ae1064c23a0eb10b37e475769b54a7cbb
MrR736 <mrr6451@outlook.com>
2024-09-05 17:55:38 -0700

    Add files via upload

--- CabMaker-Enable.cmd
+++ CabMaker-Enable.cmd
@@ -1,15 +1,15 @@
 @echo off
 cd /d %~dp0
-7za.exe x %CD%\CabMaker.7z -o%CD%\CabMaker
+7za.exe x %CD%\CabMaker.7z -o"%cd%\CabMaker"
 @Reg Add "HKEY_CLASSES_ROOT\directory\Background\shell\CabMaker" /VE /D "CabMaker" /F >Nul
-@Reg Add "HKEY_CLASSES_ROOT\directory\Background\shell\CabMaker" /f /V "Icon" /D "\"%CD%\CabMaker\Icon.ico\"
+@Reg Add "HKEY_CLASSES_ROOT\directory\Background\shell\CabMaker" /f /V "Icon" /D "\"%CD%\CabMaker\Icon.ico"
 @Reg Add "HKEY_CLASSES_ROOT\directory\Background\shell\CabMaker\command" /VE /D "Cmd.exe /c start  %CD%\CabMaker\CabMaker.exe" /F >Nul
 
 @Reg Add "HKEY_CLASSES_ROOT\Directory\shell\CabMaker" /VE /D "CabMaker" /F >Nul
-@Reg Add "HKEY_CLASSES_ROOT\Directory\shell\CabMaker" /f /V "Icon" /D "\"%CD%\CabMaker\Icon.ico\"
+@Reg Add "HKEY_CLASSES_ROOT\Directory\shell\CabMaker" /f /V "Icon" /D "\"%CD%\CabMaker\Icon.ico"
 @Reg Add "HKEY_CLASSES_ROOT\Directory\shell\CabMaker\command" /VE /D "Cmd.exe /c start  %CD%\CabMaker\CabMaker.exe" /F >Nul
 
 @Reg Add "HKEY_CLASSES_ROOT\Folder\shell\CabMaker" /VE /D "CabMaker" /F >Nul
-@Reg Add "HKEY_CLASSES_ROOT\Folder\shell\CabMaker" /f /V "Icon" /D "\"%CD%\CabMaker\Icon.ico\"
+@Reg Add "HKEY_CLASSES_ROOT\Folder\shell\CabMaker" /f /V "Icon" /D "\"%CD%\CabMaker\Icon.ico"
 @Reg Add "HKEY_CLASSES_ROOT\Folder\shell\CabMaker\command" /VE /D "Cmd.exe /c start  %CD%\CabMaker\CabMaker.exe" /F >Nul
 exit
