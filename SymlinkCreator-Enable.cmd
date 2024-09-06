a0e86908e496ec9ee3cdc6b41cfcc8911ebf104c
MrR736 <mrr6451@outlook.com>
2024-09-05 18:01:06 -0700

    Add files via upload

--- SymlinkCreator-Enable.cmd
+++ SymlinkCreator-Enable.cmd
@@ -2,14 +2,14 @@
 cd /d %~dp0
 7za.exe x %cd%\SymlinkCreator.7z -o"%cd%\SymlinkCreator"
 @Reg Add "HKEY_CLASSES_ROOT\directory\Background\shell\SymlinkCreator" /VE /D "SymlinkCreator" /F >Nul
-@Reg Add "HKEY_CLASSES_ROOT\directory\Background\shell\SymlinkCreator" /f /V "Icon" /D "\"%CD%\SymlinkCreator\Icon.ico"
+@Reg Add "HKEY_CLASSES_ROOT\directory\Background\shell\SymlinkCreator" /f /V "Icon" /D "%CD%\SymlinkCreator\Icon.ico"
 @Reg Add "HKEY_CLASSES_ROOT\directory\Background\shell\SymlinkCreator\command" /VE /D "Cmd.exe /c start  %CD%\SymlinkCreator\SymlinkCreator.exe" /F >Nul
 
 @Reg Add "HKEY_CLASSES_ROOT\Directory\shell\SymlinkCreator" /VE /D "SymlinkCreator" /F >Nul
-@Reg Add "HKEY_CLASSES_ROOT\Directory\shell\SymlinkCreator" /f /V "Icon" /D "\"%CD%\SymlinkCreator\Icon.ico"
+@Reg Add "HKEY_CLASSES_ROOT\Directory\shell\SymlinkCreator" /f /V "Icon" /D "%CD%\SymlinkCreator\Icon.ico"
 @Reg Add "HKEY_CLASSES_ROOT\Directory\shell\SymlinkCreator\command" /VE /D "Cmd.exe /c start  %CD%\SymlinkCreator\SymlinkCreator.exe" /F >Nul
 
 @Reg Add "HKEY_CLASSES_ROOT\Folder\shell\SymlinkCreator" /VE /D "SymlinkCreator" /F >Nul
-@Reg Add "HKEY_CLASSES_ROOT\Folder\shell\SymlinkCreator" /f /V "Icon" /D "\"%CD%\SymlinkCreator\Icon.ico"
+@Reg Add "HKEY_CLASSES_ROOT\Folder\shell\SymlinkCreator" /f /V "Icon" /D "%CD%\SymlinkCreator\Icon.ico"
 @Reg Add "HKEY_CLASSES_ROOT\Folder\shell\SymlinkCreator\command" /VE /D "Cmd.exe /c start  %CD%\SymlinkCreator\SymlinkCreator.exe" /F >Nul
 exit
