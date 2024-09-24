82ebf691f66161582d0fa6410d8ddcd25c3e8ac3
MrR736 <mrr6451@outlook.com>
2024-09-23 20:45:41 -0700

    Update Sudo-Disable.cmd

--- Sudo-Disable.cmd
+++ Sudo-Disable.cmd
@@ -4,5 +4,5 @@ TASKKILL /F /IM advancedrun.exe /T
 TASKKILL /F /IM NSudoLG.exe /T
 TASKKILL /F /IM PowerRun.exe /T
 TASKKILL /F /IM RunAsTI.exe /T
-rd /s /q "%cd%\Sudo"
+rd /s /q ".\Sudo"
 rd /s /q "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Sudo"
