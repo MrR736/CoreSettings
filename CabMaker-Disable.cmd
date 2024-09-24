3ae43f41ac18117d918d7ce7eecff803dd858257
MrR736 <mrr6451@outlook.com>
2024-09-23 20:46:08 -0700

    Update CabMaker-Disable.cmd

--- CabMaker-Disable.cmd
+++ CabMaker-Disable.cmd
@@ -3,5 +3,5 @@ cd /d %~dp0
 reg.exe delete "HKEY_CLASSES_ROOT\directory\shell\CabMaker" /f
 reg.exe delete "HKEY_CLASSES_ROOT\directory\Background\shell\CabMaker" /f
 reg.exe delete "HKEY_CLASSES_ROOT\Folder\shell\CabMaker" /f
-rd /s /q "%cd%\CabMaker"
+rd /s /q ".\CabMaker"
 exit
