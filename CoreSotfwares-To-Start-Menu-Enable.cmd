55854b646ca0a7b294dd68c000ba51e8fb450cd4
MrR736 <mrr6451@outlook.com>
2024-10-04 06:02:03 -0700

    Delete CoreSotfwares-To-Start-Menu-Enable.cmd

--- CoreSotfwares-To-Start-Menu-Enable.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-rd /s /q "%ProgramData%\Microsoft\Windows\Start Menu\Programs\CoreSotfwares"
-.\7z.exe x %cd%\CoreSotfwares-Shortcuts.7z -o"%ProgramData%\Microsoft\Windows\Start Menu\Programs\CoreSotfwares"
-exit
